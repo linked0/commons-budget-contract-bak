7045652dfc28a0d8e7c3908523c2cf86b8ddaefd
mukeunkim <mukeunkim@gmail.com>
2022-06-09 16:09:39 +0900

    Update test code

--- test/CommonsBudget.test.ts
+++ test/CommonsBudget.test.ts
@@ -2,30 +2,30 @@ import chai from "chai";
 import { solidity } from "ethereum-waffle";
 import { BigNumber } from "ethers";
 import { ethers, waffle } from "hardhat";
-import { CommonsBudgetContract } from "../typechain";
+import { CommonsBudget } from "../typechain";
 
 import * as assert from "assert";
 
 chai.use(solidity);
 
 describe("Test of Commons Budget Contract", () => {
-    let contract: CommonsBudgetContract;
+    let contract: CommonsBudget;
 
     const provider = waffle.provider;
     const [admin] = provider.getWallets();
     const amount = BigNumber.from(10).pow(18);
 
     before(async () => {
-        const CommonsBudgetContractFactory = await ethers.getContractFactory("CommonsBudgetContract");
-        contract = await CommonsBudgetContractFactory.deploy() as CommonsBudgetContract;
+        const CommonsBudgetFactory = await ethers.getContractFactory("CommonsBudget");
+        contract = (await CommonsBudgetFactory.deploy()) as CommonsBudget;
         await contract.deployed();
     });
 
     it("Send", async () => {
-         await provider.getSigner(admin.address).sendTransaction({
-             to: contract.address,
-             value: amount,
-         });
+        await provider.getSigner(admin.address).sendTransaction({
+            to: contract.address,
+            value: amount,
+        });
     });
 
     it("Check", async () => {
