4984520184bd546f4cd1914e70532593a09e5e84
Hyunjae Lee <linked0@gmail.com>
2022-09-30 17:12:47 +0900

    Add utility functions and scripts

--- scripts/checkBudget.ts
+++ scripts/checkBudget.ts
@@ -5,6 +5,8 @@
 // Runtime Environment's members available in the global scope.
 
 import chai, { assert, expect } from "chai";
+import fs from "fs";
+import {join} from "path";
 import { GasPriceManager } from "../utils/GasPriceManager";
 
 import { BigNumber, Wallet } from "ethers";
@@ -18,30 +20,59 @@ async function main() {
 
     const provider = ethers.provider;
     const commonsBudget = await commonsBudgetFactory.attach(process.env.COMMONS_BUDGET_CONTRACT || "");
-    const adminWallet = new Wallet(process.env.ADMIN_KEY || "", provider);
-    const managerWallet = new Wallet(process.env.MANAGER_KEY || "", provider);
-    const voteraWallet = new Wallet(process.env.VOTE_KEY || "", provider);
+    const [admin, voteManager, user, manager, proposer, ...validators] = await ethers.getSigners();
 
     const cent = BigNumber.from(10).pow(18);
     const commonsBalance = BigNumber.from(await ethers.provider.getBalance(commonsBudget.address));
-    console.log("========== Format: {BOA} . {CENT} ==========");
+    console.log("========== Balance: {BOA} . {CENT} ==========");
+
+    // CommonsBudget balance
     console.log(
-        "CommonsBudget balance: ",
+        "CommonsBudget (", commonsBudget.address, ") :",
         commonsBalance.div(cent).toString(),
         ".",
         commonsBalance.mod(cent).toString()
     );
-    const adminBalance = BigNumber.from(await ethers.provider.getBalance(adminWallet.address));
-    console.log("Admin         balance: ", adminBalance.div(cent).toString(), ".", adminBalance.mod(cent).toString());
-    const managerBalance = BigNumber.from(await ethers.provider.getBalance(managerWallet.address));
+
+    // Admin balance
+    const adminBalance = BigNumber.from(await ethers.provider.getBalance(admin.address));
+    console.log("Admin         (", admin.address, ") :", adminBalance.div(cent).toString(), ".", adminBalance.mod(cent).toString());
+
+    // Manager balance
+    const managerBalance = BigNumber.from(await ethers.provider.getBalance(manager.address));
     console.log(
-        "Manager       balance: ",
+        "Manager       (", manager.address, ") :",
         managerBalance.div(cent).toString(),
         ".",
         managerBalance.mod(cent).toString()
     );
-    const voteraBalance = BigNumber.from(await ethers.provider.getBalance(voteraWallet.address));
-    console.log("Votera        balance: ", voteraBalance.div(cent).toString(), ".", voteraBalance.mod(cent).toString());
+
+    // Votera balance
+    const voteraBalance = BigNumber.from(await ethers.provider.getBalance(voteManager.address));
+    console.log("Votera        (", voteManager.address, ") :", voteraBalance.div(cent).toString(), ".", voteraBalance.mod(cent).toString());
+
+    // Proposal balance
+    const proposerBalance = BigNumber.from(await ethers.provider.getBalance(proposer.address));
+    console.log(
+        "Proposal      (", proposer.address, ") :",
+        proposerBalance.div(cent).toString(),
+        ".",
+        proposerBalance.mod(cent).toString()
+    );
+
+    // Part of validators
+    // let index:number = 0;
+    // const validator_count: number = Number(process.env.VALIDATOR_COUNT || "0");
+    // for (const val of validators.slice(0, validator_count)) {
+    //     const valBalance = BigNumber.from(await ethers.provider.getBalance(val.address));
+    //     console.log(
+    //         "Validator", index++,
+    //         "(", val.address, "):",
+    //         valBalance.div(cent).toString(),
+    //         ".",
+    //         valBalance.mod(cent).toString()
+    //     );
+    // }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
